5592454f5e1073731230cb5579ff3e6058eed63f
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-13 23:03:15 +0700

    Add-todo-React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
-// import './App.css';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -14,6 +16,20 @@ function App() {
         <Outlet>
         </Outlet>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
+      <ToastContainer />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,58 @@
-import { Button, Input, Tooltip } from 'antd';
+import { Button, Input, Tooltip, Table, Space } from 'antd';
 import { useState } from 'react';
-import { InfoCircleOutlined, UserOutlined,CompassOutlined} from '@ant-design/icons';
+import { InfoCircleOutlined, UserOutlined, CompassOutlined, MoneyCollectOutlined } from '@ant-design/icons';
+import ColumnGroup from 'antd/es/table/ColumnGroup';
+import Column from 'antd/es/table/Column';
+import './Home.scss';
+import {toast } from 'react-toastify';
 const Home = (props) => {
-    const [name, setName] = useState('Kimoon');
-    const [address, setAddress] = useState('Moon');
-    const [showInfo, setShowInfo] = useState(false);
+    const [id, setId] = useState('');
+    const [title, setTitle] = useState('');
+    const [salary, setSalary] = useState('');
+    const [todos, setTodos] = useState([
+        { key: 'todo1', id: 'todo1', title: 'Watching Kimoon ', salary: '200' },
+        { key: 'todo2', id: 'todo2', title: 'Playing game with Kimoon ', salary: '300' },
+        { key: 'todo3', id: 'todo3', title: 'Fix bug with Kimoon ', salary: '500' },
+    ]);
+
 
     const handdleEvenClick = () => {
-        setName(address);
-        setAddress(name);
-        setShowInfo(true);
+        if (!id || !title || !salary) {
+            toast.error('Empty input');
+            return;
+        }
+        const NewTodo = {
+            key: Math.floor(Math.random() * 1000),
+            id,
+            title,
+            salary,
+        };
+        setTodos([...todos, NewTodo]);
+        toast.success('Add todo successfully !');
+
+        setId('');
+        setTitle('');
+        setSalary('');
+
     }
-    const handleOnchangeName = (event) => {
-        setName(event.target.value);
-        console.log(event.target.value);
+    const handleOnchangeID = (event) => {
+        setId(event.target.value);
     }
-    const handleOnchangeAddress = (event) => {
-        setAddress(event.target.value);
-        console.log(event.target.value);
+    const handleOnchangeTitle = (event) => {
+        setTitle(event.target.value);
+    }
+    const handleOnchangeSalary = (event) => {
+        setSalary(event.target.value);
     }
 
     return (
-        <div>
-            <div className='m-3'>Hi guy!My name is {name}</div>
-            <div className='m-3'>I'm from {address}</div>
+        <div className='homepage-container'>
             <Input
-                value={name}
-                onChange={(event) => { handleOnchangeName(event) }}
+                value={id}
+                onChange={(event) => { handleOnchangeID(event) }}
                 className='m-3'
                 style={{ width: '200px' }}
-                placeholder="Enter your name"
+                placeholder="Enter your ID"
                 prefix={<UserOutlined className="site-form-item-icon" />}
                 suffix={
                     <Tooltip title="Extra information">
@@ -38,11 +61,11 @@ const Home = (props) => {
                 }
             />
             <Input
-                value={address}
-                onChange={(event) => { handleOnchangeAddress(event) }}
+                value={title}
+                onChange={(event) => { handleOnchangeTitle(event) }}
                 className='m-3'
                 style={{ width: '200px' }}
-                placeholder="Enter your address"
+                placeholder="Enter your title"
                 prefix={<CompassOutlined className="site-form-item-icon" />}
                 suffix={
                     <Tooltip title="Extra information">
@@ -50,12 +73,35 @@ const Home = (props) => {
                     </Tooltip>
                 }
             />
+            <Input
+                value={salary}
+                onChange={(event) => { handleOnchangeSalary(event) }}
+                className='m-3'
+                style={{ width: '200px' }}
+                placeholder="Enter your salary"
+                prefix={<MoneyCollectOutlined className="site-form-item-icon" />}
+                suffix={
+                    <Tooltip title="Extra information">
+                        <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                    </Tooltip>
+                }
+            />
             <Button type="default" onClick={(event) => { handdleEvenClick(event) }}>Click me</Button>
-            {showInfo && (
-                <div className='m-3'>
-                    Additional information: My name :{name} - {address}
-                </div>
-            )}
+            <Table className='m-5' dataSource={todos}>
+                <Column title='ID' dataIndex='id' key='key' />
+                <Column title='Title' dataIndex='title' key='key' />
+                <Column title='Salary' dataIndex='salary' key='key' />
+                <Column
+                    title="Action"
+                    key="action"
+                // render={(_, record) => (
+                //     <Space size="middle">
+                //         <a>Invite</a>
+                //         <a>Delete</a>
+                //     </Space>
+                // )}
+                />
+            </Table>
         </div>
     )
 }
