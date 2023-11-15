22437f6633b1a611db010385059be362ea46b6cf
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-11-15 11:47:08 +0700

    Loading-Data-React

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,46 +1,67 @@
 import './User.scss';
-import { Button, Input, Tooltip, Table, Space } from 'antd';
+import { Button, Input, Tooltip, Table, Space, Spin } from 'antd';
 import Column from 'antd/es/table/Column';
-import { InfoCircleOutlined, UserOutlined, CompassOutlined, MoneyCollectOutlined,DeleteOutlined } from '@ant-design/icons';
-import { useState,useEffect } from 'react';
+import { InfoCircleOutlined, UserOutlined, CompassOutlined, MoneyCollectOutlined, DeleteOutlined, LoadingOutlined } from '@ant-design/icons';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
 
 const User = (props) => {
-    const [dataUser,setDataUser] = useState([]);
+    const [dataUser, setDataUser] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        let fetchData=async()=> {
-          let res = await axios.get('https://reqres.in/api/users?page=2');
-          let data = res && res.data && res.data.data ? res.data.data : [];
-          setDataUser(data);
-          console.log('check',res.data.data);
-        }
-        fetchData();
-      }, []);
+        setTimeout(async () => {
+            let fetchData = async () => {
+                let res = await axios.get('https://reqres.in/api/users?page=2');
+                let data = res && res.data && res.data.data ? res.data.data : [];
+                setDataUser(data.reverse());
+                setLoading(false)
+            }
+            fetchData();
+        }, 2000)
+    }, []);
     return (
         <div className="list-container-user">
-        <div className='' style={{ color: '#06f7ff' }}>List User Components</div>
-            <Table className='mx-5 pt-5' dataSource={dataUser} rowKey='id'>
-                <Column title='Email' dataIndex='email'  />
-                <Column title='First name' dataIndex='first_name'  />
-                <Column title='Last name' dataIndex='last_name'  />
-                <Column
-                    title="Action"
-                    key="action"
-                    render={(_, record) => (
-                        <Space size="middle">
-                            <Tooltip title="Xóa">
-                                <Button
-                                    shape="circle"
-                                    icon={<DeleteOutlined />}
+            <div className='' style={{ color: '#06f7ff' }}>List User Components</div>
+            {loading === false ?
+                <Table className='mx-5 pt-5' dataSource={dataUser} rowKey='id'>
+                    <Column title='Email' dataIndex='email' />
+                    <Column title='First name' dataIndex='first_name' />
+                    <Column title='Last name' dataIndex='last_name' />
+                    <Column
+                        title="Action"
+                        key="action"
+                        render={(_, record) => (
+                            <Space size="middle">
+                                <Tooltip title="Xóa">
+                                    <Button
+                                        shape="circle"
+                                        icon={<DeleteOutlined />}
                                     // onClick={() => handleDeleteTodo(record.key)}
-                                />
-                            </Tooltip>
-                        </Space>
-                    )}
-                />
-            </Table>
+                                    />
+                                </Tooltip>
+                            </Space>
+                        )}
+                    />
+                </Table>
+                :
+                <div>
+                    <span>Loading...</span>
+                    <Spin
+
+                        indicator={
+
+                            <LoadingOutlined
+                                style={{
+                                    fontSize: 24,
+                                }}
+                                spin
+                            />
+                        }
+                    />
+                </div>
+            }
         </div>
     )
 }
